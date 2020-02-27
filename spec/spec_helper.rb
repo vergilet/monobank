108aaab152432e483f0b64fe29a69fb4732db31e
vergilet <yoslavskiy@innocode.no>
2020-02-27 09:33:37 +0200

    Created spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'monobank'
+
+ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.join(ENGINE_RAILS_ROOT, 'spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
